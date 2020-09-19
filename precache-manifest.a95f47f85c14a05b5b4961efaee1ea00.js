3146f75d86498268539c594926a11bfe765d1803
Jaime Garcia <jaimegarcia@ingeniosolido.com>
2020-09-19 12:53:01 -0500

    Updates

---
+++ precache-manifest.a95f47f85c14a05b5b4961efaee1ea00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f74646bd47e13c229a57f34fec1fc0",
+    "url": "/youtube-search/index.html"
+  },
+  {
+    "revision": "89ecf6ce28f2662ffd89",
+    "url": "/youtube-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e3e443350485fbce2de0",
+    "url": "/youtube-search/static/js/2.aad47d69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-search/static/js/2.aad47d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ecf6ce28f2662ffd89",
+    "url": "/youtube-search/static/js/main.d704ac58.chunk.js"
+  },
+  {
+    "revision": "b7c2a2d8518dd704823f",
+    "url": "/youtube-search/static/js/runtime-main.4f39e33c.js"
+  },
+  {
+    "revision": "90cd07f5d407fa2b1f59e5dd34a912e6",
+    "url": "/youtube-search/static/media/logo.90cd07f5.png"
+  }
+]);
